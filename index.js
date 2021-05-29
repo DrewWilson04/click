be8c1bb7b3e23dbb3901d49d2b3fdca4f52b7b28
drew <drewwilson2022@gmail.com>
2021-05-29 12:01:37 -0500

    now using local storage

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var clicks = 0;
+document.getElementById("clicks").innerHTML = localStorage.getItem("clicks");
+var clicks = Number(localStorage.getItem("clicks")) || 0
 
 function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
+  localStorage.setItem("clicks", clicks);
+  document.getElementById("clicks").innerHTML = localStorage.getItem("clicks");
 };
 
 document.addEventListener('click', function () {
